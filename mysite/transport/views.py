0d3209d3294171da40f9cb32829495fd09113b1b
Stella Silverstein <stella.silversteim@isotoma.com>
2016-03-17 10:57:28 +0000

    add geospatial data and example

--- mysite/transport/views.py
+++ mysite/transport/views.py
@@ -1,8 +1,5 @@
 from django.http import HttpResponse
 
-# Create your views here.
 
 def index(request):
-    return HttpResponse('This page shows a list of most recent posts.')
-
-
+    return HttpResponse("Hello, world. You're at the transport index.")
