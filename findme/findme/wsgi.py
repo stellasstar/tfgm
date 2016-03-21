879ceab4f4912ac8c67cd32d4f63b60a8b07c908
Stella Silverstein <stella.silversteim@isotoma.com>
2016-03-21 10:20:05 +0000

    rename project to findme

--- findme/findme/wsgi.py
+++ findme/findme/wsgi.py
@@ -1,16 +1,14 @@
 """
-WSGI config for findme project.
+WSGI config for googlemaps project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "googlemaps.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "findme.settings")
-
 application = get_wsgi_application()
