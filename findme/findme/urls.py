5a31f12b62d8f346705ea866ebc8bda413566412
Stella Silverstein <stella.silversteim@isotoma.com>
2016-03-21 11:35:06 +0000

    trying to add login page

--- findme/findme/urls.py
+++ findme/findme/urls.py
@@ -4,4 +4,8 @@ from django.contrib import admin
 urlpatterns = [
               url(r'^admin/', include(admin.site.urls)),
               url(r'', include('waypoints.urls'), name="waypoints"),
+              url(r'^login/$',
+        'django.contrib.auth.views.login',
+        name='login',
+        kwargs={'template_name': 'findme/login.html'})
 ]
