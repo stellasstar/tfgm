82bcdd729f692a9f2b8874a9bbc26f87730d4669
Stella Silverstein <stella.silversteim@isotoma.com>
2016-03-22 13:22:06 +0000

    trying to modify the default user

--- findme/findme/settings.py
+++ findme/findme/settings.py
@@ -117,5 +117,11 @@ LOGIN_URL = 'http://localhost:8000/'
 # if user is not logged in then it will redirect the user to login page.
 django.contrib.auth.LOGIN_URL = '/'
 
-#AUTH_USER_MODEL = "gatekeeper.UserProfile"
+AUTH_USER_MODEL = 'gatekeeper.User'
+AUTH_PROFILE_MODULE = 'gatekeeper.User' 
+
+AUTHENTICATION_BACKENDS = (
+    'django_facebook.auth_backends.FacebookBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
 
--- findme/gatekeeper/models.py
+++ findme/gatekeeper/models.py
@@ -1,9 +1,83 @@
-# -*- coding: utf-8 -*-
+
 from django.db import models
-from django.conf import settings
+
+import re
+import uuid
+
+from django.core import validators
+from django.utils import timezone
+from django.core.mail import send_mail
+from django.utils.http import urlquote
 from django.utils.translation import ugettext_lazy as _
-from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django import forms
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save 
+from django.conf import settings 
+from django.dispatch import receiver 
 
-from . import managers
+class UserManager(BaseUserManager):
 
+    def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
+        now = timezone.now()
+        if not username:
+            raise ValueError(_('The given username must be set'))
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email,
+                          is_staff=is_staff, is_active=False,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        return self._create_user(username, email, password, False, False,
+                                 **extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        user=self._create_user(username, email, password, True, True,
+                               **extra_fields)
+        user.is_active=True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL) 
+    username = models.CharField(_('username'), max_length=30, unique=True,
+                                help_text=_('Required. 30 characters or fewer. Letters, numbers and @/./+/-/_ characters'),
+                                validators=[
+                                    validators.RegexValidator(re.compile('^[\w.@+-]+$'), _('Enter a valid username.'), _('invalid'))
+                                ])
+    first_name = models.CharField(_('first name'), max_length=30, blank=True, null=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True, null=True)
+    email = models.EmailField(_('email address'), max_length=255)
+    is_staff = models.BooleanField(_('staff status'), default=False,
+                                   help_text=_('Designates whether the user can log into this admin site.'))
+    is_active = models.BooleanField(_('active'), default=False,
+                                    help_text=_('Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email',]
+    
+    objects = UserManager()
+    
+    
+    def __str__(self):  
+        return "%s's profile" % self.user      
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None):
+        send_mail(subject, message, from_email, [self.email])
+        
