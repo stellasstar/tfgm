d3a9d1400f08b093647bad88847d4113ca7a2160
Stella Silverstein <stella.silverstein@isotoma.com>
2016-03-30 14:47:12 +0100

    fixed the db scripts

--- findme/findme/scripts/builddb.sh
+++ findme/findme/scripts/builddb.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ROLE=geouser 
-PASSWORD=geodatabase 
-sudo -u postgres psql -c "CREATE ROLE $ROLE PASSWORD '$PASSWORD' NOSUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN" 
-sudo -u postgres psql -c "CREATE DATABASE $ROLE WITH OWNER = $ROLE ENCODING = 'UTF8'" 
-sudo -u postgres psql --dbname=$ROLE -c "CREATE EXTENSION postgis"
-sudo -u postgres psql --dbname=$ROLE -c "CREATE EXTENSION postgis_topology;"
+NAME=geodatabase
+USER=geouser 
+PASSWORD=geopassword
+sudo -u postgres psql -c "CREATE ROLE $USER PASSWORD '$PASSWORD' NOSUPERUSER CREATEDB NOCREATEROLE INHERIT LOGIN" 
+sudo -u postgres psql -c "CREATE DATABASE $NAME WITH OWNER = $USER ENCODING = 'UTF8'" 
+sudo -u postgres psql --dbname=$NAME -c "CREATE EXTENSION postgis"
+sudo -u postgres psql --dbname=$NAME -c "CREATE EXTENSION postgis_topology;"
--- findme/setup.py
+++ findme/setup.py
@@ -5,17 +5,38 @@ version = '0.0.1dev0'
 
 setup(name='findme',
       version=version,
-      author="Isotoma Limited",
-      author_email="support@isotoma.com",
+      author="Stella Silverstein",
+      author_email="stella.silverstein@isotoma.com",
       license="Apache Software License",
       packages=find_packages(exclude=['ez_setup']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-        'pastescript'
+        'Django',
+        'Markdown',
+        'Paste',
+        'PasteDeploy',
+        'PasteScript',
+        'Pillow',
+        'PyJWT',
+        'argparse',
+        'django-awesome-avatar',
+        'django-filter',
+        'djangorestframework',
+        'flake8',
+        'mccabe',
+        'oauthlib',
+        'pep8',
+        'psycopg2',
+        'pyflakes',
+        'python-openid',
+        'python-social-auth',
+        'requests',
+        'requests-oauthlib',
+        'six',
+        'wsgiref'
       ],
       entry_points = """
       [paste.paster_create_template]
-      isotoma_django=isotoma_django.template:DjangoProjectTemplate
       """
       )
