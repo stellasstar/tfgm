4c3a78a068063da309f0da61916d6128dffcb97c
Stella Silverstein <stella.silversteim@isotoma.com>
2016-04-01 14:52:53 +0100

    broken tests

--- findme/gatekeeper/tests.py
+++ findme/gatekeeper/tests.py
@@ -20,39 +20,49 @@ class TestUserBase(TestCase):
         self._user_counter = 0
         self.created_users = list()
         # delete the users
+        print User
         User.objects.all().delete()
         
         # related profile must have been delete as well
-        profiles = UserProfile.objects.all()
+        profiles = models.UserProfile.objects.all()
         self.assertEquals(len(profiles), 0)        
 
-    def create_user(self):
+    def create_user_profile(self):
         first_name = 'delete'
         last_name = 'me'
         i = self._user_counter
         username = '%s%s%d' % (first_name, last_name, i)
         self._user_counter += 1
-
+        
+        # create a userprofile when a user is created
         user = User.objects.create(
             email='%s@example.com' % username,
             username=username,
             first_name=first_name,
             last_name=last_name,
         )
+        
+        print user
 
+        # related profile has to exist
+        profile = models.UserProfile.objects.get(user) 
+        
         self.created_users.append(user)
-        return user    
+        return (user, profile)
 
 class TestUserProfileModel(tests.TestUserBase):
     
     def setUp(self):
         super(TestUserProfileModel, self).setUp()
         TestUserBase.setUp(self)
-        self.user = self.create_user()
+        (self.user, self.profile) = self.create_user_profile()
         
     def test_user_profile(self):
-        # Check that a Profile instance has been crated
-        self.assertIsInstance(self.user.UserProfile, models.UserProfile)
+        u = self.user
+        p = self.profile
+        # Check that a Profile instance has been created
+        self.assertEquals(p,u)
+        self.assertIsInstance(p, models.UserProfile)
         # Call the save method of the user to activate the signal
         # again, and check that it doesn't try to create another
         # profile instace
@@ -63,12 +73,21 @@ class TestUserRegistrationView(tests.TestUserBase):
     def setUp(self):  
         self.client = Client()
         TestUserBase.setUp(self)
-        self.user = self.create_user()
+        (self.user, self.profile) = self.create_user_profile()
+        self.user.save()
         
     def test_user_register(self):
-        response = self.client.get('/register/')
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Register Your Account')
-
+        # Get login page
+        response = self.client.get('/gatekeeper/register/', follow=True)
+        self.assertTrue(response, 'Register' in response.content)
+        
+        # Check response code
+        self.assertEquals(response.status_code, 200)       
+    
         response = self.client.post('/register/', (self.user))
         self.assertEqual(response.status_code, 200)
+        
+    def test_user_login(self):
+        # Check 'Log in' in response
+        response = self.client.get('/gatekeeper/login/', follow=True)
+        self.assertTrue('Log in' in response.content)         
